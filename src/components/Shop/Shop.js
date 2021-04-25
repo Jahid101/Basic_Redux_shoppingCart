35081bd865dc2c99d55bf9820d3696a6bd1a1cd5
Jahid101 <jahidhasananik.official@gmail.com>
2021-04-25 21:00:11 +0600

    Redux basic

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,7 +4,6 @@ import { addToCart } from '../../redux/actions/cartActions';
 import Product from '../Product/Product';
 
 const Shop = (props) => {
-    console.log(props);
     const {products, addToCart} = props;
 
     return (
@@ -25,7 +24,6 @@ const Shop = (props) => {
 
 const mapStateToProps = state => {
     return {
-        cart: state.cart,
         products: state.products,
     }
 }
